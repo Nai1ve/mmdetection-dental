7162edacbe9355cda5e0d0bdfe75eed4eb55cb28
Lpz <Lpz970821@163.com>
2025-11-19 14:34:36 +0800

    在分类损失函数中添加数据传递方法

--- mmdet/models/roi_heads/bbox_heads/ccsw_bbox_head.py
+++ mmdet/models/roi_heads/bbox_heads/ccsw_bbox_head.py
@@ -1,16 +1,23 @@
 # 实现一个 类别条件的空间加权损失函数（Class-Conditional Spatially-Weighted Loss）
 
 import torch
-import torch.nn.functional as F
-from mmengine import ConfigDict, MMLogger
-from torch import Tensor
-from typing import Tuple, Optional, List
 
-from mmdet.registry import MODELS
-from .convfc_bbox_head import Shared2FCBBoxHead
 from mmdet.models.losses import accuracy
 from ...task_modules import SamplingResult
-from mmdet.structures.bbox import get_box_tensor
+from mmengine import MMLogger
+
+from .convfc_bbox_head import Shared2FCBBoxHead
+from torch import Tensor
+from typing import List, Optional, Tuple, Union
+from mmengine.config import ConfigDict
+from mmdet.models.layers import multiclass_nms
+from mmdet.registry import MODELS
+from mmdet.models.utils import empty_instances
+from mmdet.utils import  InstanceList
+from mmengine.structures import InstanceData
+import torch.nn.functional as F
+from mmdet.structures.bbox import get_box_tensor, scale_boxes
+
 
 @MODELS.register_module()
 class CCSWBBOXHead(Shared2FCBBoxHead):
@@ -233,3 +240,248 @@ class CCSWBBOXHead(Shared2FCBBoxHead):
                     losses['loss_bbox'] = bbox_pred[pos_inds].sum()
 
             return losses
+
+
+    def forward(self, x: Tuple[Tensor]) -> tuple:
+        """Forward features from the upstream network.
+        Args:
+            x (tuple[Tensor]): Features from the upstream network, each is
+                a 4D-tensor.
+
+        Returns:
+            tuple: A tuple of classification scores and bbox prediction.
+
+                - cls_score (Tensor): Classification scores for all \
+                    scale levels, each is a 4D-tensor, the channels number \
+                    is num_base_priors * num_classes.
+                - bbox_pred (Tensor): Box energies / deltas for all \
+                    scale levels, each is a 4D-tensor, the channels number \
+                    is num_base_priors * 4.
+                - feature_embed (Tensor): Feature embedding for all \
+        """
+        # shared part
+        if self.num_shared_convs > 0:
+            for conv in self.shared_convs:
+                x = conv(x)
+
+        if self.num_shared_fcs > 0:
+            if self.with_avg_pool:
+                x = self.avg_pool(x)
+
+            x = x.flatten(1)
+
+            for fc in self.shared_fcs:
+                x = self.relu(fc(x))
+        # separate branches
+        x_cls = x
+        x_reg = x
+
+        for conv in self.cls_convs:
+            x_cls = conv(x_cls)
+        if x_cls.dim() > 2:
+            if self.with_avg_pool:
+                x_cls = self.avg_pool(x_cls)
+            x_cls = x_cls.flatten(1)
+        for fc in self.cls_fcs:
+            x_cls = self.relu(fc(x_cls))
+
+        for conv in self.reg_convs:
+            x_reg = conv(x_reg)
+        if x_reg.dim() > 2:
+            if self.with_avg_pool:
+                x_reg = self.avg_pool(x_reg)
+            x_reg = x_reg.flatten(1)
+        for fc in self.reg_fcs:
+            x_reg = self.relu(fc(x_reg))
+        # 特征信息是x_cls
+        cls_score = self.fc_cls(x_cls) if self.with_cls else None
+        bbox_pred = self.fc_reg(x_reg) if self.with_reg else None
+        return cls_score, bbox_pred,x_cls
+
+
+    def predict_by_feat(self,
+                        rois: Tuple[Tensor],
+                        cls_scores: Tuple[Tensor],
+                        x_cls: Tuple[Tensor],
+                        bbox_preds: Tuple[Tensor],
+                        batch_img_metas: List[dict],
+                        rcnn_test_cfg: Optional[ConfigDict] = None,
+                        rescale: bool = False,
+                        bbox_feats:Tuple[Tensor]=None) -> InstanceList:
+        """Transform a batch of output features extracted from the head into
+        bbox results.
+
+        Args:
+            rois (tuple[Tensor]): Tuple of boxes to be transformed.
+                Each has shape  (num_boxes, 5). last dimension 5 arrange as
+                (batch_index, x1, y1, x2, y2).
+            cls_scores (tuple[Tensor]): Tuple of box scores, each has shape
+                (num_boxes, num_classes + 1).
+            bbox_preds (tuple[Tensor]): Tuple of box energies / deltas, each
+                has shape (num_boxes, num_classes * 4).
+            batch_img_metas (list[dict]): List of image information.
+            rcnn_test_cfg (obj:`ConfigDict`, optional): `test_cfg` of R-CNN.
+                Defaults to None.
+            rescale (bool): If True, return boxes in original image space.
+                Defaults to False.
+
+        Returns:
+            list[:obj:`InstanceData`]: Instance segmentation
+            results of each image after the post process.
+            Each item usually contains following keys.
+
+                - scores (Tensor): Classification scores, has a shape
+                  (num_instance, )
+                - labels (Tensor): Labels of bboxes, has a shape
+                  (num_instances, ).
+                - bboxes (Tensor): Has a shape (num_instances, 4),
+                  the last dimension 4 arrange as (x1, y1, x2, y2).
+        """
+        MMLogger.debug(MMLogger.get_current_instance(),"进入添加视觉特征的ConvFCBBoxHeadAddVisualFeature.predict_by_feat方法")
+        assert len(cls_scores) == len(bbox_preds)
+        result_list = []
+        for img_id in range(len(batch_img_metas)):
+            img_meta = batch_img_metas[img_id]
+            results = self._predict_by_feat_single_add_visual_feature(
+                roi=rois[img_id],
+                cls_score=cls_scores[img_id],
+                x_cls=x_cls[img_id],
+                bbox_pred=bbox_preds[img_id],
+                img_meta=img_meta,
+                rescale=rescale,
+                rcnn_test_cfg=rcnn_test_cfg,
+                bbox_feat = bbox_feats[img_id])
+            MMLogger.debug(MMLogger.get_current_instance(),"-----------------results_info_begin------------------------")
+            MMLogger.debug(MMLogger.get_current_instance(),results)
+            MMLogger.debug(MMLogger.get_current_instance(),"-----------------results_info_end--------------------------")
+            result_list.append(results)
+
+        return result_list
+
+    def _predict_by_feat_single_add_visual_feature(
+            self,
+            roi: Tensor,
+            cls_score: Tensor,
+            x_cls: Tensor,
+            bbox_pred: Tensor,
+            img_meta: dict,
+            rescale: bool = False,
+            rcnn_test_cfg: Optional[ConfigDict] = None,
+            bbox_feat : Tensor = None ) -> InstanceData:
+        """Transform a single image's features extracted from the head into
+        bbox results.
+
+        Args:
+            roi (Tensor): Boxes to be transformed. Has shape (num_boxes, 5).
+                last dimension 5 arrange as (batch_index, x1, y1, x2, y2).
+            cls_score (Tensor): Box scores, has shape
+                (num_boxes, num_classes + 1).
+            bbox_pred (Tensor): Box energies / deltas.
+                has shape (num_boxes, num_classes * 4).
+            img_meta (dict): image information.
+            rescale (bool): If True, return boxes in original image space.
+                Defaults to False.
+            rcnn_test_cfg (obj:`ConfigDict`): `test_cfg` of Bbox Head.
+                Defaults to None
+
+        Returns:
+            :obj:`InstanceData`: Detection results of each image\
+            Each item usually contains following keys.
+
+                - scores (Tensor): Classification scores, has a shape
+                  (num_instance, )
+                - labels (Tensor): Labels of bboxes, has a shape
+                  (num_instances, ).
+                - bboxes (Tensor): Has a shape (num_instances, 4),
+                  the last dimension 4 arrange as (x1, y1, x2, y2).
+        """
+        results = InstanceData()
+        if roi.shape[0] == 0:
+            return empty_instances([img_meta],
+                                   roi.device,
+                                   task_type='bbox',
+                                   instance_results=[results],
+                                   box_type=self.predict_box_type,
+                                   use_box_type=False,
+                                   num_classes=self.num_classes,
+                                   score_per_cls=rcnn_test_cfg is None)[0]
+
+        # some loss (Seesaw loss..) may have custom activation
+        if self.custom_cls_channels:
+            scores = self.loss_cls.get_activation(cls_score)
+        else:
+            scores = F.softmax(
+                cls_score, dim=-1) if cls_score is not None else None
+
+        img_shape = img_meta['img_shape']
+        num_rois = roi.size(0)
+        # bbox_pred would be None in some detector when with_reg is False,
+        # e.g. Grid R-CNN.
+        if bbox_pred is not None:
+            num_classes = 1 if self.reg_class_agnostic else self.num_classes
+            roi = roi.repeat_interleave(num_classes, dim=0)
+            bbox_pred = bbox_pred.view(-1, self.bbox_coder.encode_size)
+            bboxes = self.bbox_coder.decode(
+                roi[..., 1:], bbox_pred, max_shape=img_shape)
+        else:
+            bboxes = roi[:, 1:].clone()
+            if img_shape is not None and bboxes.size(-1) == 4:
+                bboxes[:, [0, 2]].clamp_(min=0, max=img_shape[1])
+                bboxes[:, [1, 3]].clamp_(min=0, max=img_shape[0])
+
+        if rescale and bboxes.size(0) > 0:
+            assert img_meta.get('scale_factor') is not None
+            scale_factor = [1 / s for s in img_meta['scale_factor']]
+            bboxes = scale_boxes(bboxes, scale_factor)
+
+        # Get the inside tensor when `bboxes` is a box type
+        bboxes = get_box_tensor(bboxes)
+        box_dim = bboxes.size(-1)
+        bboxes = bboxes.view(num_rois, -1)
+
+        if rcnn_test_cfg is None:
+            # This means that it is aug test.
+            # It needs to return the raw results without nms.
+            results.bboxes = bboxes
+            results.scores = scores
+        else:
+            # 获取到保留的掩码
+            det_bboxes, det_labels,keep_idx = multiclass_nms(
+                bboxes,
+                scores,
+                rcnn_test_cfg.score_thr,
+                rcnn_test_cfg.nms,
+                rcnn_test_cfg.max_per_img,
+                box_dim=box_dim,
+                return_inds=True
+            )
+            results.bboxes = det_bboxes[:, :-1]
+            results.scores = det_bboxes[:, -1]
+            results.labels = det_labels
+            # 转换
+            proposal_inds = keep_idx // self.num_classes
+
+            # 使用keep_idx 筛选出匹配的视觉特征和完整概率
+            final_all_class_probs = scores[proposal_inds]
+            final_visual_features = bbox_feat[proposal_inds]
+            final_x_cls = x_cls[proposal_inds]
+
+            results.set_field(
+                value=final_visual_features,
+                name='visual_features',
+                dtype=Tensor,
+            )
+
+            results.set_field(
+                value=final_all_class_probs,
+                name='all_class_probs',
+                dtype=Tensor,
+            )
+
+            results.set_field(
+                value=final_x_cls,
+                name='x_cls',
+                dtype=Tensor,
+            )
+
+        return results
